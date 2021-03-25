38488400c6b0d438f7b74526ee0637dffb5af975
Taras Didukh <didkh86@gmail.com>
2021-03-25 19:01:13 +0200

    New IP

--- src/app/services/token-interceptor.service.ts
+++ src/app/services/token-interceptor.service.ts
@@ -14,7 +14,7 @@ import { throwError } from 'rxjs';
   providedIn: 'root'
 })
 export class TokenInterceptorService implements HttpInterceptor {
-  baseUrl = 'http://18.198.88.98:8080';
+  baseUrl = 'http://3.65.244.12:8080';
   allowedUrl = ['/login', '/login/', '/login/request-password', '/login/request-password/', '/login/password', '/login/password/'];
 
   constructor(
